6958a8b2f1c4402986e234afb1537fda0732b117
Your Name <northstaraokeystone@gmail.com>
2025-10-23 17:50:14 -0700

    fix(build): Implement complete Jest testing environment
    Signed-off-by: Your Name <northstaraokeystone@gmail.com>

---
+++ frontend/jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less)$': 'identity-obj-proxy', // Mocks CSS imports
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
